eea696c4e242d7e0ab18f3081d1ed6369e4d420e
Kasper Meilgaard <kasper@borges.xyz>
2017-09-25 16:27:47 +0200

    landing page scaffold

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import Header from '../Header'
+
+import './Home.css'
+
+class Home extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  componentWillMount() {
+
+  }
+
+  render() {
+    return (
+      <div className="Home">
+        
+      </div>
+    );
+  }
+}
+
+export default Home
