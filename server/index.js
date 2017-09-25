f28b1f8851e0c88b97b0114f9b79f2c1ec5529c2
Kasper Meilgaard <kasper@borges.xyz>
2017-09-25 11:44:25 +0200

    update server for mongo connection, integrate express, morgan and router

--- server/index.js
+++ server/index.js
@@ -1,23 +1,33 @@
-const express = require('express');
-const path = require('path');
-
-const app = express();
-const PORT = process.env.PORT || 3000;
-
+// Main starting point of the application
+const express    = require('express');
+const http       = require('http');
+const bodyParser = require('body-parser'); //middleware which parses HTTP request bodies and makes them available in req.body
+const morgan     = require('morgan');      //HTTP request logger middleware
+const app        = express();
+const router     = require('./router');
+const mongoose   = require('mongoose');
+const cors       = require('cors');
+const PORT       = process.env.PORT || 3000;
+const path       = require('path');
 // Static files
 app.use(express.static(path.resolve(__dirname, '../client/build_webpack')));
 
-// Api
-app.get('/api', function (req, res) {
-  res.set('Content-Type', 'application/json');
-  res.send('{"message":"Api is ready for requests"}');
-});
+// DB Setup
+mongoose.connect(process.env.MONGODB_URI);
+
+// App Setup
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));  //parse bodies of all incoming requests into JSON
+
+// Allow cross-origin resource sharing
+app.use(cors());
+app.options('*', cors());
+
+// Application Routes
+router(app);
 
 
-app.get('*', function(request, response) {
-  response.sendFile(path.resolve(__dirname, '../client/build_webpack', 'index.html'));
-});
-
-app.listen(PORT, function () {
-  console.log(`Listening on port ${PORT}`);
-});
+// Server Setup
+const server = http.createServer(app);
+server.listen(PORT);
+console.log('Server listening on:', PORT);
